f07d3d2ca9f39384b3ebf0b77c43a3d56ff4c1b6
Greg Manco <gregmanco@gmail.com>
2019-06-29 18:37:09 -0400

    Completed Psychic Word Game, Changed peusdocode to fit the code that was written Added CSS and Bootstrap elements to index.html Corrected errors in javascript that would display the whole array ins

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,95 +2,95 @@
 var letters = ["a", "b", "c"];
 
 // Create an empty array to hold what the user guesses
-var UserGuess = []
+var userGuess = [];
 // Create a variable that will be randomly assigned one of the three letters
-var computerGuess = letters[Math.floor(Math.random() * letters.length )];
-console.log(computerGuess); 
+var computerGuess = letters[Math.floor(Math.random() * letters.length)];
+console.log(computerGuess);
 // Set a limit for the user guesses left and assign that to a variable. 
-var guessLeft = 10
+var guessLeft = 3;
 // Create counters for wins and losses and assign them the value of zero
 var wins = 0;
 var losses = 0;
 
-// Create three functions to update guesses, update guesses left, and update guesses so far
 
-  // In the update guesses left function create a code to grab the HTML element and setting it equal to the guesses left.
-  // (i.e. guesses left will get displayed in HTML)
-  
 
- 
-  // In the update guesses function we get a random letter-to-guess and assign it based on a random generator (only looking at a, b, or c)
-  if (UserGuess == computerGuess ) {
-      win++
-      attempts -=
-      UserGuess.push()
-  }
-  
- 
-  // In the update guesses-so-far function we take the guesses the user has tried -- then display it as letters separated by commas.
-
-// Create a reset function will be called when we reset everything
-// The reset Function should assign the values of the variables to their original values before the user started to play
-// For example, the guesses left variable, the guessed letter empty array ...etc
-
-// Call the functions that you created above
-
-// Create a document.onkeydown function that will capture the keyboard clicks.
-    document.onkeydown = function(event) {
-   UserGuess= event.key;
-   console.log(event);
+// Creates a function that will generate a random variable from the letters array
+function updateGuess() {
+  computerGuess = letters[Math.floor(Math.random() * letters.length)];
 }
 
+// In the update guesses-so-far function we take the guesses the user has tried -- then display it as letters separated by commas.
 
-  // This function should reduce the guesses by one
+
+// Creates a function that will be called upon to start the game. Sets beginning game parameters.
+ function start(){
+ document.getElementById("wins").textContent = wins;
+ document.getElementById("losses").textContent = losses;
+ document.getElementById("guess-left").textContent = guessLeft;
+ }
+// Creates a function that will reset the game parameters so the game will continue after a win/loss.
+function reset() {
+  guessLeft = 3
+  userGuess = [];
+  console.log(guessLeft);
+  document.getElementById("guess-left").textContent = guessLeft;
+}
+
+// Call the start function to begin the game.
+
+ start();
+
+// Create a document.onkeydown function that will capture the keyboard clicks.
+document.onkeyup = function (event){ 
+  var userChoice = event.key;
+  console.log(event);
+
 
   // Set the event.key to Lowercase the letter and save it in a variable 
-  var userChoice = event.toLowerCase();
-  console.log(event);
+  userChoice = event.key.toLowerCase();
+  console.log(userChoice);
   // Then push the guess to the guessedLetters array
+  userGuess.push(userChoice);
+  // Checks the user's guess vs the computers guess. Increases win by 1, decreases guesses left by 1, and calls for reset.
+  if (userChoice === computerGuess) {
+    wins++;
+    guessLeft--;
+    console.log(userGuess);
+    document.getElementById("wins").textContent = wins;
+    updateGuess();
+    document.getElementById("user-guess").textContent = "";
+    document.getElementById("guess-left").textContent = guessLeft;
+    document.getElementById("user-guess").textContent = userChoice;
+    console.log(computerGuess);
+    reset();
+//  If user guess isnt the same as the computer's guess and guesses left is not 0 it will decrease
+// guesses left by -1
+  } else if (userChoice != computerGuess && guessLeft != 0) {
+    guessLeft--;
+    document.getElementById("user-guess").textContent = "";
+    document.getElementById("guess-left").textContent = guessLeft;
+    // If user is out of guesses increase losses by +1 and call for reset.
+  } else {
+    losses++
+    guessLeft--;
+    document.getElementById("guess-left").textContent = guessLeft;
+    document.getElementById("losses").textContent = losses;
+    document.getElementById("user-guess").textContent = "";
+    document.getElementById("user-guess").textContent = userChoice;
+    updateGuess();
 
-  // Then its going to run the update functions
+    reset();
 
-
-  // We create an if-statement to check if there's a match.
-
-
-    // If there is then we win and we'll update the HTML to display the win.
-
-    // Then we'll reset the game
-
-  // If we are out of guesses...
-
-    // Then we will loss and we'll update the HTML to display the loss.
-
-    // Then we'll call the reset
-
-
-
-
-
-
-
-
-
-
-
-    /*for (i= 0; i<10; i++) {
-document.getElementById("g-left");
-    if( UserGuess==letters) {
-        wins++
-        Guessleft--
-        document.getElementById("g-left").innerHTML="GuessLeft";
-        document.getElementById("wins").innerHTML="wins";
-        console.log(Guessleft);
-        console.log(wins);*/
+  }
 
 
+  // Created for loop to display the userGuess array.
+
+  for (var i = 0; i < userGuess.length; i++) {
+    document.getElementById("user-guess").textContent += userGuess[i] + ", ";
+  }
 
 
-      /* for ( i = 0; i < letters.length; i++)
-        if (computerGuess && UserGuess == -1 ) {
-        lettersGuessed.push(UserGuess); 
-        guessLeft -= 1;
-        document.getElementById("letters-g").innerHTML= lettersGuessed;
-        document.getElementById("g-left").innerHTML = guessLeft; */
+ 
+
+}
